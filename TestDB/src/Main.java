cd1750542dad85aaecd4d9d4b34b8f63fc131938
Ivan Burychka <ivan.burychka@gmail.com>
2018-01-13 22:02:33 +0200

    lesson 282

--- TestDB/src/Main.java
+++ TestDB/src/Main.java
@@ -1,6 +1,7 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,9 +11,29 @@ public class Main {
 	
 		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:G:\\Program\\Java\\TestDB\\testjava.db");
 				Statement statement = (Statement) conn.createStatement();) {
+//			conn.setAutoCommit(false);
+			statement.execute("CREATE TABLE IF NOT EXISTS contacts "
+							+ "(name TEXT, phone INTEGER, email TEXT)");
+//			statement.execute("INSERT INTO contacts (name, phone, email) "
+//							+ "VALUES ('Joe', 45632, 'joe@enywhere.com')");
+//			statement.execute("INSERT INTO contacts (name, phone, email) "
+//							+ "VALUES ('Jane', 4829484, 'jane@somewhere.com')");
+//			statement.execute("INSERT INTO contacts (name, phone, email) "
+//							+ "VALUES ('Fido', 9038, 'dog@email.com')");
+//			statement.execute("UPDATE contacts SET phone = 5566789 WHERE name = 'Jane' "); 
+//			statement.execute("DELETE FROM contacts WHERE name = 'Joe'");
 			
-			String sql = "CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT );";
-			statement.execute(sql);
+			statement.execute("SELECT * FROM contacts");
+			ResultSet result = statement.getResultSet();
+			while (result.next()) {
+				System.out.println(result.getString("name") + " " +
+									result.getInt("phone") + " " +
+									result.getString("email"));
+			}
+			result.close();
+			
+			
+			
 		} catch (SQLException e) {
 			System.out.println("Something is wrong: " + e.getMessage());
 		} 
