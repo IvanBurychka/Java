074379949b6ddff54f352145f892f9b1f14d3a23
Ivan Burychka <ivan.burychka@gmail.com>
2018-01-13 21:04:26 +0200

    after 281 lesson

--- TestDB/src/Main.java
+++ TestDB/src/Main.java
@@ -1,13 +1,18 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 	
 	public static void main(String[] args) {
 	
-		try {
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:G:\\Program\\Java\\dataBases\\testjava.db");
+		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:G:\\Program\\Java\\TestDB\\testjava.db");
+				Statement statement = (Statement) conn.createStatement();) {
+			
+			String sql = "CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT );";
+			statement.execute(sql);
 		} catch (SQLException e) {
 			System.out.println("Something is wrong: " + e.getMessage());
 		} 
