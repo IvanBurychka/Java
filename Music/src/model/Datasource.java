a3b307ab74d2e9b0bdc50bf2e639a6da17dda79a
Ivan Burychka <ivan.burychka@gmail.com>
2018-01-14 21:00:46 +0200

    Begining of DB Music

---
+++ Music/src/model/Datasource.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Datasource {
+
+	public static final String DB_NAME = "music.db";
+	public static final String CONNECTION_STRING = "jdbc:sqlite:g:\\Program\\Java\\Music\\" + DB_NAME;
+	
+	public static final String TABLE_ALBUMS = "albums"; 
+	public static final String COLUMN_ALBUM_ID = "_id";
+	public static final String COLUMN_ALBUM_NAME = "name";
+	public static final String COLUMN_ALBUM_ARTIST = "artist";
+	
+	public static final String TABLE_ARTISTS = "artist";
+	public static final String
+	public static final String
+	
+	public static final String
+	public static final String
+	public static final String
+	public static final String
+	
+}
